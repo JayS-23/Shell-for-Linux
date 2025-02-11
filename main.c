f9bf59da4232db6048f16958dbbf16c586d223b8
Jay Nayanbhai Sanghani <jy483624@dal.ca>
2025-02-11 17:33:14 -0400

    Added history function for child process

--- main.c
+++ main.c
@@ -4,15 +4,17 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <errno.h>
+
 #define MAX_LINE 80 /* The maximum length command */
 #define MAX_History_Length 10 // Maximum length for history array
+
 int main(void)
 {
     char *args[MAX_LINE/2 + 1]; /* an array of character strings */
     int should_run = 1; /* flag to determine when to exit program */
     char input[80];
-    char *historyArr[MAX_History_Length];
-    pid_t pidArr[MAX_History_Length];
+    char *historyArr[MAX_History_Length] = {NULL};
+    pid_t pidArr[MAX_History_Length] = {NULL};
     int index =0;
 
     while(should_run) {
@@ -20,6 +22,7 @@ int main(void)
         printf("CSCI3120>");
         fflush(stdout);
         fgets(input, 80, stdin);
+        historyArr[index] = input;
 
         char* token;
         token = strtok(input, " \n");
@@ -46,8 +49,18 @@ int main(void)
         }
         if (pid == 0) {
             printf("Child process created successfully with PID: %d\n",getpid() );
-            if (execvp(args[0], args) == -1) {
-                // printf("Not a valid command!!\n");
+            if (strcmp(args[0], "history") == 0) {
+                printf("%-6s %-6s %-6s\n", "ID", "PID", "Command");
+                int i = index-1;
+                for (int i=0, curr = index-1; i<MAX_History_Length; i++) {
+                    printf("%-6d %-6s %-6s\n", i, pidArr[curr] , historyArr[curr]);
+
+                    curr = (curr + 9) % 10; // Its basically curr - 1, and then to remove the negative values
+                                            // and to wrap around I took %10 after adding 10 to it.
+                }
+
+            }
+            else if (execvp(args[0], args) == -1) {
                 printf("Error: %s\n", strerror(errno));
                 exit(1);
             }
@@ -55,14 +68,9 @@ int main(void)
         else {
             printf("Child process created successfully with Chile PID: %d\n",pid );
             int status;
-
             waitpid(pid, &status, 0);
-            if (index >= MAX_History_Length) {
-                index = 0;
-                historyArr[index] = args;
-                pidArr[index] = pid;
-                index++;
-            }
+            pidArr[index] = pid;
+            index = (index + 1) % MAX_History_Length;
         }
     }
     return 0;
